0b288b96dd486b614374d30af66df708449a78f9
RohilDubey <xevoker@hotmail.com>
2016-10-03 10:18:04 -0400

    Add files via upload

--- sysc3303/src/sysc3303/TFTPClient.java
+++ sysc3303/src/sysc3303/TFTPClient.java
@@ -1,5 +1,4 @@
 package sysc3303;
-
 //TFTPClient.java
 //This class is the client side for a very simple assignment based on TFTP on
 //UDP/IP. The client uses one port and sends a read or write request and gets 
@@ -12,243 +11,256 @@ import java.util.*;
 
 public class TFTPClient extends TFTPHost{
 
+    private DatagramSocket sendReceiveSocket;
+    public static final int READ= 1; 
+    public static final int WRITE = 2;
 
-private DatagramSocket sendReceiveSocket;
+    public static String DEFAULT_FILE_PATH = "src/sysc3303/files/";
+    private String filePath;
+    
+    private int sendPort;
+    private Mode run;
+    byte requestFormatRead = 1;
+    //we can run in normal (send directly to server) or test
+    //(send to simulator) mode
+    public static enum Mode { NORMAL, TEST};
 
-public static final int READ= 1; 
-public static final int WRITE = 2;
+    public TFTPClient()
+    {
+        super();
+        try {
 
-private int sendPort;
-private Mode run;
+            // Construct a datagram socket and bind it to any available
+            // port on the local host machine. This socket will be used to
+            // send and receive UDP Datagram packets.
+            sendReceiveSocket = new DatagramSocket();
+        } catch (SocketException se) {   // Can't create the socket.
+            se.printStackTrace();
+            System.exit(1);
+        }
+        run=Mode.NORMAL;
+    }
 
-//we can run in normal (send directly to server) or test
-//(send to simulator) mode
-public static enum Mode { NORMAL, TEST};
+    public void sendAndReceive(int type)
+    {
+        byte[] msg = new byte[100], // message we send
+        fn, // filename as an array of bytes
+        md, // mode as an array of bytes
+        data; // reply as array of bytes
+        String filename, mode="Octet"; // filename and mode as Strings
+        int j, len;
 
-	public TFTPClient()
-	{
-		super();
-		 try {
-			 
-		    // Construct a datagram socket and bind it to any available
-		    // port on the local host machine. This socket will be used to
-		    // send and receive UDP Datagram packets.
-		    sendReceiveSocket = new DatagramSocket();
-		 } catch (SocketException se) {   // Can't create the socket.
-		    se.printStackTrace();
-		    System.exit(1);
-		 }
-		 run=Mode.NORMAL;
-	}
-	
-	public void sendAndReceive(int type)
-	{
-		 byte[] msg = new byte[100], // message we send
-		        fn, // filename as an array of bytes
-		        md, // mode as an array of bytes
-		        data; // reply as array of bytes
-		 String filename, mode="Octet"; // filename and mode as Strings
-		 int j, len;
-		 
-		 		 
-		 if (run==Mode.NORMAL) 
-		    sendPort = 69;
-		 else
-		    sendPort = 23;
-		 
-		 sc.reset();
-		 System.out.println("Please enter a filename");
-		 filename=sc.next();
-		 
-		 
-		 
-		 fn=filename.getBytes();
-		 md=mode.getBytes();
-		 
-		 //form a request with fileame, format, and message type
-		 
-		 msg=formatRequest(fn,md,type);
-		 	
-		 len = fn.length+md.length+4; // length of the message
-		   // length of filename + length of mode + opcode (2) + two 0s (2)
-		 
-		
-		   // Construct a datagram packet that is to be sent to a specified port
-		   // on a specified host.
-		   // The arguments are:
-		   //  msg - the message contained in the packet (the byte array)
-		   //  the length we care about - k+1
-		   //  InetAddress.getLocalHost() - the Internet address of the
-		   //     destination host.
-		   //     In this example, we want the destination to be the same as
-		   //     the source (i.e., we want to run the client and server on the
-		   //     same computer). InetAddress.getLocalHost() returns the Internet
-		   //     address of the local host.
-		   //  69 - the destination port number on the destination host.
-		   try {
-		      sendPacket = new DatagramPacket(msg, len,
-		                          InetAddress.getLocalHost(), sendPort);
-		   } catch (UnknownHostException e) {
-		      e.printStackTrace();
-		      System.exit(1);
-		   }
-		
-		   printOutgoingInfo(sendPacket, "Client",verbose);
-		   
-		
-		   // Send the datagram packet to the server via the send/receive socket.
-		
-		   try {
-		      sendReceiveSocket.send(sendPacket);
-		   } catch (IOException e) {
-		      e.printStackTrace();
-		      System.exit(1);
-		   }
-		
-		   System.out.println("Client: Packet sent.");
-		
-		   // Construct a DatagramPacket for receiving packets up
-		   // to 100 bytes long (the length of the byte array).
-		
-		   data = new byte[516];
-		   receivePacket = new DatagramPacket(data, data.length);
-		
-		   System.out.println("Client: Waiting for packet.");
-		   try {
-		      // Block until a datagram is received via sendReceiveSocket.
-		      sendReceiveSocket.receive(receivePacket);
-		   } catch(IOException e) {
-		      e.printStackTrace();
-		      System.exit(1);
-		   }
-		
-		  // Process the received datagram.
-		   while(!shutdown){
-				if (type==WRITE) {
-					try {
-						byte[] resp = new byte[4];
-						receivePacket = new DatagramPacket(resp,4);
-						while (timeout) {
-							timeout = false;
-							try {
-								sendReceiveSocket.setSoTimeout(300);
-								sendReceiveSocket.receive(receivePacket);
-							} catch (SocketTimeoutException e) {
-								timeout = true;
-								if (shutdown) {
-									System.exit(0);
-								}
-							}
-						}
-						sendPort = receivePacket.getPort();
-						BufferedInputStream in = new BufferedInputStream(new FileInputStream(filename));
-						read(in,sendReceiveSocket,sendPort);
-						in.close();
+        if (run==Mode.NORMAL) 
+            sendPort = 69;
+        else
+            sendPort = 23;
 
-					} catch (FileNotFoundException e) {
-						e.printStackTrace();
-					}
-					catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-				else if (type==READ) {
-					filename = "copy".concat(filename); //avoid overwriting the existing file
-					try {
-						BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename));
-						write(out,sendReceiveSocket);
-						out.close();
+        sc.reset();
+        System.out.println("Please enter a filename");
+        filename=sc.next();
 
-					}
-					catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-		   printIncomingInfo(receivePacket,"Client",verbose);
-		      
-		   System.out.println();
-		
-		 } // end of loop
-		
-		 // We're finished, so close the socket.
-		 sendReceiveSocket.close();
-	}
+         
+        fn=filename.getBytes();
+        md=mode.getBytes();
 
-	
-	/*
-	 * formatRequest takes a filename and a format and an opcode (which corresponds to read or write)
-	 * and formats them into a correctly formatted request
-	 */
-	public byte[] formatRequest(byte[] filename, byte[] format, int opcode) {
-		int lf = filename.length,lm=format.length;
-		
-		byte [] result=new byte[lf+4+lm];
-		
-		result[0] =(byte) 0;
-		result[1] = (byte) opcode;
-		
-		System.arraycopy(filename,0,result,2,lf);
-		
-		result[lf+2] = 0;
+        //form a request with fileame, format, and message type
 
-		System.arraycopy(format,0,result,3+lf,lm);
-		
-		result[lf+3+lm] = 0;
-		
-		return result;
-	}
-	
-	public void promptUser(){
-		
-		String x;
-		
-		do{
-			System.out.println("(R)ead, (w)rite, (o)ptions, or (q)uit?");
-			x = sc.next();
-			if (x.contains("R")||x.contains("r")) {
-				sc.reset();
-				this.sendAndReceive(READ);
-				//System.exit(0);
+        msg=formatRequest(fn,md,type);
+
+        len = fn.length+md.length+4; // length of the message
+        // length of filename + length of mode + opcode (2) + two 0s (2)
+
+        // Construct a datagram packet that is to be sent to a specified port
+        // on a specified host.
+        // The arguments are:
+        //  msg - the message contained in the packet (the byte array)
+        //  the length we care about - k+1
+        //  InetAddress.getLocalHost() - the Internet address of the
+        //     destination host.
+        //     In this example, we want the destination to be the same as
+        //     the source (i.e., we want to run the client and server on the
+        //     same computer). InetAddress.getLocalHost() returns the Internet
+        //     address of the local host.
+        //  69 - the destination port number on the destination host.
+        try {
+            sendPacket = new DatagramPacket(msg, len,
+                InetAddress.getLocalHost(), sendPort);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        printOutgoingInfo(sendPacket, "Client",verbose);
+
+        // Send the datagram packet to the server via the send/receive socket.
+        try {
+            sendReceiveSocket.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        System.out.println("Client: Packet sent.");
+
+       //reset timetout
+        timeout=true;
+        
+        // Process the received datagram.
+        while(!shutdown){
+            if (type==WRITE) {
+                try {
+                    byte[] resp = new byte[4];
+                    receivePacket = new DatagramPacket(resp,4);
+                    while (timeout) {
+                        timeout = false;
+                        try {
+                            sendReceiveSocket.setSoTimeout(10000);
+                            sendReceiveSocket.receive(receivePacket);
+                        } catch (SocketTimeoutException e) {
+                            timeout = true;
+                            if (shutdown) {
+                                System.exit(0);
+                            }
+                        }
+                    }
+                    printIncomingInfo(receivePacket,"Client",verbose);
+                    if (resp[0]==(byte)0 && resp[1]==(byte)4 && resp[2]==(byte)0 && resp[3]==(byte)0){
+                        //ACK 0 received 
+                        	sendPort = receivePacket.getPort();
+                        	System.out.println(System.getProperty("user.dir"));
+                            BufferedInputStream in = new BufferedInputStream(new FileInputStream(filename));
+                            read(in,sendReceiveSocket,sendPort, (byte)requestFormatRead);
+                            timeout = false;
+                            in.close();
+                    }
+                    else {//Server didn't answer correctly
+                    	System.out.println("First Ack invalid, shutdown");
+                    	System.exit(0);
+                    }
+                    
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (type==READ) {
+                filename = "copy".concat(filename); //avoid overwriting the existing file
+                try {
+                	
+                    BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename));
+                    write(out,sendReceiveSocket);
+                    out.close();
+
+                }
+                catch (IOException e) {
+                    e.printStackTrace();                }
+            }
+            
+
+            promptUser();
+
+        } // end of loop
+
+        // We're finished, so close the socket.
+        sendReceiveSocket.close();
+    }
+
+    /*
+     * formatRequest takes a filename and a format and an opcode (which corresponds to read or write)
+     * and formats them into a correctly formatted request
+     */
+    public byte[] formatRequest(byte[] filename, byte[] format, int opcode) {
+        int lf = filename.length,lm=format.length;
+
+        byte [] result=new byte[lf+4+lm];
+
+        result[0] =(byte) 0;
+        result[1] = (byte) opcode;
+
+        System.arraycopy(filename,0,result,2,lf);
+
+        result[lf+2] = 0;
+
+        System.arraycopy(format,0,result,3+lf,lm);
+
+        result[lf+3+lm] = 0;
+
+        return result;
+    }
+
+    public void promptUser(){
+
+        String x;
+
+        do{
+            System.out.println("(R)ead, (w)rite, (o)ptions, or (q)uit?");
+            x = sc.next();
+            if (x.contains("R")||x.contains("r")) {
+                sc.reset();
+                this.sendAndReceive(READ);
+                //System.exit(0);
+            }
+            else if (x.contains("w")||x.contains("W")) {
+                sc.reset();
+                this.sendAndReceive(WRITE);
+                //System.exit(0);
+            }
+            else if (x.contains("q")||x.contains("Q")) {
+                this.sendReceiveSocket.close();
+                System.exit(0);
+            }
+            else if (x.contains("o")||x.contains("O")) {
+                System.out.println("Would you like to turn off verbose mode? Y/N");
+                x = sc.next();
+                sc.reset();
+                if (x.contains("y")||x.contains("Y")) {
+                    this.verbose = false;
+                }
+                System.out.println("Would you like to turn on test mode?");
+                x = sc.next();
+                sc.reset();
+                if (x.contains("y")||x.contains("Y")) {
+                    this.run=Mode.TEST;
+                }
+
+                System.out.println("(R)ead or (w)rite?");
+                sc.reset();
+            }
+            else {
+                sc.reset(); //clear scanner
+            }
+        }while(sc.hasNext()) ;
+        sc.close();
+
+    }
+
+    public static void main(String args[])
+    {
+    	Scanner s = new Scanner(System.in);
+        TFTPClient c = new TFTPClient();
+        while(true) {
+        	System.out.println("------------------------------------------------------");
+        	System.out.println("File path selection: \n");
+			System.out.println("Enter the name of the file path for the Client to use (if * is typed then src/client/files/ will be used):");
+			
+			c.filePath = s.next();
+			
+			if (c.filePath.trim().equals("*")) {
+				c.filePath = DEFAULT_FILE_PATH;
 			}
-			else if (x.contains("w")||x.contains("W")) {
-				sc.reset();
-				this.sendAndReceive(WRITE);
-				//System.exit(0);
+			s.reset();
+			//Check that file path exists/is a valid directory
+			if (new File(c.filePath).isDirectory()) {
+				break;
+			} else {
+				System.out.println("File path does not exist or is not a directory, try again.");
 			}
-			else if (x.contains("q")||x.contains("Q")) {
-				this.sendReceiveSocket.close();
-				System.exit(0);
-			}
-			else if (x.contains("o")||x.contains("O")) {
-				System.out.println("Would you like to turn off verbose mode? Y/N");
-				x = sc.next();
-				sc.reset();
-				if (x.contains("y")||x.contains("Y")) {
-					this.verbose = false;
-				}
-				System.out.println("Would you like to turn on test mode?");
-				x = sc.next();
-				sc.reset();
-				if (x.contains("y")||x.contains("Y")) {
-					this.run=Mode.TEST;
-				}
-				
-				System.out.println("(R)ead or (w)rite?");
-				sc.reset();
-			}
-			else {
-				sc.reset(); //clear scanner
-			}
-		}while(sc.hasNext()) ;
-		sc.close();
-		
-	}
-	public static void main(String args[])
-	{
-		TFTPClient c = new TFTPClient();
-		c.promptUser();
-		
-	}
+        }
+        c.promptUser();
+
+    }
 }
-
 
--- sysc3303/src/sysc3303/TFTPServerListener.java
+++ sysc3303/src/sysc3303/TFTPServerListener.java
@@ -1,105 +1,103 @@
 package sysc3303;
-
-
 //TFTPServerListener.java
 //This class is the listener process of the server. The Listener use only one socket (69) and wait for any request.
 //Once a request is received the listener create a new thread to handle the request.
 //Before waiting again a new request the listener check the shutdown command(status)
 //If no request is received during tOut, the listener check the shutdown command.
- 
 
-
+/*error code : 05 opcode : 01=filenotfound/02=access violation /03=disk full  or allocation exceed/04=file already exists
+ * + force to happen no error sim , opcode +errorcode + usefull message +0 byte and stop*/
 
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.*;
 
 public class TFTPServerListener extends TFTPHost {
 
-	// UDP datagram packets and sockets used to send / receive
-	private DatagramPacket receivePacket;
-	private DatagramSocket receiveSocket;
-	
-	//timeout
-	private final int tOut=300;//10 sec
-	
-	//status (shutdown or not)
-	private boolean status;
-	
-	public TFTPServerListener()
-	{
-		super();
-	   try {
-	      // Construct a datagram socket and bind it to port 69
-	      // on the local host machine. This socket will be used to
-	      // receive UDP Datagram packets.
-	      receiveSocket = new DatagramSocket(69);
-	   } catch(SocketException se) {
-	      se.printStackTrace();
-	      System.exit(1);
-	   }
-	   try {
-		   receiveSocket.setSoTimeout(tOut);
-	   }catch (SocketException set){
-		   set.printStackTrace();
-		   System.exit(1);
-	   }
-	   
-	   status=false;
-	   
-	}
-	
-	public void Listen(){
-		byte[] data;
-		
-		for (;;){
-			
-			data = new byte[100];
-			receivePacket = new DatagramPacket(data, data.length);
+    // UDP datagram packets and sockets used to send / receive
+    private DatagramPacket receivePacket;
+    private DatagramSocket receiveSocket;
 
-		      System.out.println("ServerListener: Waiting for packet.");
-		      // Block until a datagram packet is received from receiveSocket or the timeout expires
-		      try {
-		         receiveSocket.receive(receivePacket);
-		      } catch (InterruptedIOException t){
-		    	  //no request is received during tOut, the listener check the shutdown command.
-		    	  System.out.println("No file transfer requested");
-		    	  checkStat();
-		      }catch (IOException e) {
-		         e.printStackTrace();
-		         System.exit(1);
-		      } 
-		      
-		    // create a new thread to handle the file transfer request
-		      Thread handler=new Thread(new TFTPServerHandler(receivePacket));
-		      handler.start();
-		      
-		    //Before waiting again a new request the listener check the shutdown command(status)
-		     
-		      checkStat();
-		}
-		
-	}
-	
-	public void checkStat(){
-		
-		if (shutdown){
-			System.out.println("ServerListener shutdown , no more file transfer request possible");
-			//maybe need to wait all the thread to end? 
-			System.exit(1);
-		}
-		
-	}
-	
-	public static void main( String args[] ) throws Exception
-	{
-	   TFTPServerListener c = new TFTPServerListener(); 
-	   c.Listen();
-	   
-	}
-	
-	
-	
+    //timeout
+    private final int tOut=10000;//10 sec
+
+    //status (shutdown or not)
+    private boolean status;
+
+    public TFTPServerListener()
+    {
+        super();
+        try {
+            // Construct a datagram socket and bind it to port 69
+            // on the local host machine. This socket will be used to
+            // receive UDP Datagram packets.
+            receiveSocket = new DatagramSocket(69);
+        } catch(SocketException se) {
+            se.printStackTrace();
+            System.exit(1);
+        }
+        try {
+            receiveSocket.setSoTimeout(tOut);
+        }catch (SocketException set){
+            set.printStackTrace();
+            System.exit(1);
+        }
+
+        status=false;
+
+    }
+
+    public void Listen(){
+        byte[] data;
+        // Current version will prompt during every loop iteration however in future version with GUI
+        // A dedicated text box will exist so the prompt is only displayed once
+        System.out.println("In order to shutdown the server, press (Y) during the prompt.");
+        for (;;){
+
+            data = new byte[100];
+            receivePacket = new DatagramPacket(data, data.length);
+
+            System.out.println("ServerListener: Waiting for packet.");
+            // Block until a datagram packet is received from receiveSocket or the timeout expires
+            try {
+                receiveSocket.receive(receivePacket);
+            } catch (InterruptedIOException t){
+                //no request is received during tOut, the listener check the shutdown command.
+                System.out.println("No file transfer requested");
+                //checkStat();
+            }catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            } 
+            // Before creating a new thread, check if shutdown has been intitiated or not. 
+            //checkStat();
+            // create a new thread to handle the file transfer request
+            Thread handler=new Thread(new TFTPServerHandler(receivePacket));
+            handler.start();
+        }
+
+    }
+
+    public void checkStat(){
+        String x;
+        // Ask to shutdown 
+        System.out.println("Shutdown the server? (Y|N)");
+        x = sc.next();
+        // If operator says yes then the server shutsdown as threads act independently of the server once created.
+        if (x.contains("Y")){
+            System.out.println("Server is now shutting down.");
+            sc.reset();
+            System.exit(0);
+        }
+    }
+
+    public static void main( String args[] ) throws Exception
+    {
+        TFTPServerListener c = new TFTPServerListener(); 
+        c.Listen();
+    }
+
 }
