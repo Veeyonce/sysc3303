6cd9ec498ca633c7607a7fa70dacb4f60a242fc7
Lisa <Lisa@172.17.197.205>
2016-10-13 17:10:33 -0400

    add while true in server

--- sysc3303/src/sysc3303/TFTPServer.java
+++ sysc3303/src/sysc3303/TFTPServer.java
@@ -10,16 +10,19 @@ public class TFTPServer {
         String x;
         Scanner sc= new Scanner (System.in);
         // Ask to shutdown 
-        System.out.println("In order to shutdown the server, press (Y) during the prompt.");
-        System.out.println("Shutdown the server? (Y|N)");
-        x = sc.next();
-        // If operator says yes then the server shutdown as threads act independently of the server once created.
-        if (x.contains("Y")|| x.contains("y")){
-            System.out.println("Server is now shutting down.");
-            sc.reset();
-            ((TFTPServerListener) c).setStatus();
-            c.interrupt();
-            System.exit(0);
+        while(true){
+        	System.out.println("In order to shutdown the server, press (Y) during the prompt.");
+	        System.out.println("Shutdown the server? (Y|N)");
+	        x = sc.next();
+	        // If operator says yes then the server shutdown as threads act independently of the server once created.
+	        if (x.contains("Y")|| x.contains("y")){
+	            System.out.println("Server is now shutting down.");
+	            sc.reset();
+	            ((TFTPServerListener) c).setStatus();
+	            c.interrupt();
+	            System.exit(0);
+	        }
         }
+        
 	}
 }
